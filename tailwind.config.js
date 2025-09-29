90d2a73379fea173684271e2d4b1ce965dc03b05
tdangw <tdangw@gmail.com>
2025-09-29 10:09:02 +0700

    fix: final tailwind postcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@
 export default {
   content: [
     './index.html',
+    // Quét tất cả các thư mục chứa component/code
     './{components,context,hooks,services}/**/*.{js,ts,jsx,tsx}',
+    // Quét các file .tsx/.jsx nằm trực tiếp ở thư mục gốc
     './*.{js,ts,jsx,tsx}',
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Caro-AI-Arena/', // RẤT QUAN TRỌNG: Thay đổi thành tên repo của bạn
-})
+  base: './Caro-AI-Arena/', // RẤT QUAN TRỌNG: Thay đổi thành tên repo của bạn
+});
